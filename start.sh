67795e873c391215dbee8c90fe16611dfc121994
IndiaNews Fix <fix@indianews.com>
2026-05-08 03:51:28 +0000

    Temp: enable DEBUG to diagnose 500 error

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-python manage.py migrate --run-syncdb
-python setup_demo.py
-gunicorn indianews.wsgi:application
+set -x
+echo "=== Starting migrate ==="
+python manage.py migrate --run-syncdb 2>&1
+echo "=== Running setup_demo ==="
+python setup_demo.py 2>&1
+echo "=== Starting gunicorn ==="
+gunicorn indianews.wsgi:application --log-level debug --access-logfile - --error-logfile -
